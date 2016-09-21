7ba44bfc0b0293e0bacbb003ca46670e4f79ddb8
Srivathsan Murali <srivathsan92@gmail.com>
2016-09-21 14:44:36 +0200

    my own podcast player

---
+++ pypass.py
@@ -0,0 +1,15 @@
+""" pypass - cli client
+  add pocasts
+  remove podcasts
+  list podcasts
+  update podcasts
+  list episodes
+  play range
+  play entire list
+  play reveresed
+  play specific
+  play first
+  play last
+  add to queue
+  clear queue
+"""
